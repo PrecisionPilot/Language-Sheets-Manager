51f829b8debe713e340643785396112c4666e8ae
= <69162332+PilotPrix@users.noreply.github.com>
2021-09-03 21:15:19 -0400

    Updates (See discription)
    - Sorted language options presented in output
    - Combined "Chinese" & "Chinese (Simplified)" language options
    - Formatted the output statements much nicer
    - Fail-safe: implemented anti case sensitivity, eliminated user inputted language options that didn't exist

--- Language Sheets Manager.py
+++ Language Sheets Manager.py
@@ -14,59 +14,75 @@ def formatter(path):
     languagesSelection = []
 
     # switch the languages around to the right order
-    for row in data:
-        if row[0] == "English":
-            row[0], row[1] = row[1], row[0]
-            row[2], row[3] = row[3], row[2]
+    for i in range(len(data)):
+        # data[i][0] represents the language in each row
+        if data[i][0] == "English":
+            data[i][0], data[i][1] = data[i][1], data[i][0]
+            data[i][2], data[i][3] = data[i][3], data[i][2]
+        if data[i][0] == "Chinese (Simplified)": # Make sure all is "Chiense" not "Chinese (Simplified)"
+            data[i][0] = "Chinese"
     
-    # Get all existing languages in the spreadsheet, store it on "languages" list
+    # Get all existing languages in the spreadsheet, store it in "languagesData" list
     for row in data:
-        # row[0] represents the language of the row
         if not row[0] in languagesData:
             languagesData[row[0]] = []
         # store the row on the dictionary
         languagesData[row[0]].append(row[2:4]) # get only the translation
-    
 
     # ---Exporting process---
     
-    # Ask user which language to output
-    print("select languages (seperated by spaces) to output as csv: ", end="")
+    # Ask user which language to output to csv
+    print("Select languages (seperated by spaces) to output as csv (tip: type \"all\" to use all options)\n" +
+            "Here are your options (", end="")
     for key,val in languagesData.items(): languages.append(key)
-    for i in range(len(languages) - 1):
-        print(languages[i])
-    print(languages[-1])
-    languagesSelection = input("\n").split(' ')
+    languages.sort()
+    for i in range(len(languages) - 1): # print the language options available
+        print(languages[i], end=", ")
+    print(languages[-1] + "): ", end="")
+    languagesSelection = input().split(' ') # store user's selection(s)
+    for i in range(len(languagesSelection)): # anti case sensitive code
+        languagesSelection[i] = languagesSelection[i][0].upper() + languagesSelection[i][1:].lower()
 
     # Get folder of the path
     destination_path = os.path.dirname(path)
     if destination_path: # C:\Users\seanw\Downloads\ -> Downloads/
         destination_path += "\\"
 
+
     # Export based on language selection
     exportCount = 0
-    if languagesSelection[0].lower() == "all":
+    if languagesSelection[0].lower() == "all": # type "all" to export all the languages there is
         languagesSelection = languages
-    print(languagesSelection)
+    for i in range(len(languagesSelection)): # Fail-safe mechanism: Make sure selection languages exist
+        if not languagesSelection[i] in languages:
+            languagesSelection.pop(i) # remove elemend of the languages selection that doesn't exist
+    # Export
     for languageSelection in languagesSelection:
         with open(destination_path + languageSelection + ".csv", "w+", newline="", encoding="utf-8") as f: #writes it in the right path 
             writer = csv.writer(f)
             writer.writerows(languagesData[languageSelection])
         exportCount += 1
     
-    print("Successfully exported", exportCount, "file(s)!")
-    print("Have a good day")
+    # Message for whether the export succeeded or not
+    if exportCount > 0:
+        print("Successfully exported", exportCount, "file(s)!")
+        print("Have a good day")
+    else:
+        print("No files exported, something may have gone wrong")
+# ---End of formatter()
 
-x = input("type in the path of the your tsv file to organize\n" +
-            "type \"this\" for local directory\n")
+
+x = input("Type in the path of the your tsv file to organize (tip: type \"this\" for local directory): ")
             
-if x == "this":
-    formatter(input("type in file name: "))
+if x.lower() == "this":
+    formatter(input("Type in file name: "))
 else:
     formatter(x)
 
 
-# Find a way to combine "Chinese" and "Chinese (Simplified)"
+# Find a way to combine "Chinese" and "Chinese (Simplified)" --Check
+# Fix formatting for output statements
+# Fail-safe (anti case sensitive)
 # See if commas cause problems to the csv when reading from Anki and Quizlet
 # Sort dictionary if possible
 # Perhaps save it as a tsv
