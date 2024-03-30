31258c40f1c9c63c4ae98e8646cbc3d0c6c46e9a
Sean Wang <seanwaterloo2997@gmail.com>
2024-03-30 18:49:44 -0300

    Revert "Mono row modification update"
    This reverts commit 22710dbef11cdb7eef606c1f594b3ed408395304.

--- Cantonese.py
+++ Cantonese.py
@@ -29,23 +29,50 @@ def inputMultiline() -> str:
     return userInput
 
 def formatter(data: list):
+    data1 = []
+    data2 = []
+
+    # 1st deck back: English  -  Simplified (pinyin)
+    for i in range(len(data)):
+        if not data[i][0]:
+            break
+
+        translation = translate(data[i][0])
+        # If there's already the English def given
+        if data[i][1]:
+            arr = [data[i][0], f"{data[i][1]}  -  {translation[1]} ({pinyin(translation[1])})"]
+        else:
+            arr = [data[i][0], f"{translation[0].capitalize()}  -  {translation[1]} ({pinyin(translation[1])})"]
+        data1.append(arr)
+        
+        # Print progress
+        progress = round((i + 1) * 100 / len(data))
+        print(f"Cantonese1.tsv: {progress}%", end="\r")
+    print("Cantonese1.tsv: 100%")
 
     for i in range(len(data)):
-        if not data[i]:
+        if not data[i][2]:
             break
 
         # 2nd deck front: Simplified (pinyin)
-        data[i] = [f"{chinese_converter.to_simplified(data[i])} ({pinyin(data[i])})", chinese_converter.to_traditional(data[i])]
+        arr = [f"{chinese_converter.to_simplified(data[i][2])} ({pinyin(data[i][2])})", chinese_converter.to_traditional(data[i][2])]
+        data2.append(arr)
         
         # Print progress
         progress = round((i + 1) * 100 / len(data))
         print(f"Cantonese2.tsv: {progress}%", end="\r")
     print("Cantonese2.tsv: 100%")
 
-    # Export Cantonese.tsv
-    with open("Cantonese.tsv", "w+", newline="", encoding="utf-8") as f: # Writes it in the right path 
+    # Export Cantonese1.tsv
+    with open("Cantonese1.tsv", "w+", newline="", encoding="utf-8") as f: # Writes it in the right path 
         writer = csv.writer(f, delimiter="\t")
-        writer.writerows(data)
+        writer.writerows(data1)
+
+    # Export Cantonese2.tsv
+    with open("Cantonese2.tsv", "w+", newline="", encoding="utf-8") as f: # Writes it in the right path 
+        writer = csv.writer(f, delimiter="\t")
+        writer.writerows(data2)
+    print("\nSuccessfully exported!")
 
 
 # ---End of formatter()
@@ -58,8 +85,10 @@ print("Paste in the copied speadsheet")
 userInput = inputMultiline()
 print("Loading...")
 
-# Convert to list
+# Convert to 2d list
 userInput = userInput.split("\n")
+for i, inp in enumerate(userInput):
+    userInput[i] = inp.split("\t")
 
 formatter(userInput)
 
